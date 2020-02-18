ef7e4419829818fea52c2e8d00186c72b080487e
dmitryelizavetin <dmitry.elizavetin@live.com>
2020-02-18 22:58:34 +0300

    2020-02-18

--- Assets/Scripts/UI/InBattlePage.cs
+++ Assets/Scripts/UI/InBattlePage.cs
@@ -6,15 +6,28 @@ public class InBattlePage : MonoBehaviour
     public GameController Controller;
     public Button AttackButton;
     public Button SwitchButton;
+    public Button PauseButton;
+    public Button ContinueButton;
+    public Button ReloadButton;
+    public Button MainMenuButton;
+    public GameObject VisualPartMenu;
 
     private void Awake()
     {
+        VisualPartMenu.SetActive(false);
         AttackButton.onClick.AddListener(OnAttackButtonClick);
         SwitchButton.onClick.AddListener(() => Controller.SwitchCharacter());
+        PauseButton.onClick.AddListener(OnPauseButtonClick);
     }
 
     private void OnAttackButtonClick()
     {
         Controller.PlayerMove();
     }
+
+    private void OnPauseButtonClick()
+    {
+        VisualPartMenu.SetActive(true);
+    }
+
 }
--- Assets/Scripts/UI/StartMenuPage.cs
+++ Assets/Scripts/UI/StartMenuPage.cs
@@ -4,16 +4,25 @@ using UnityEngine.UI;
 public class StartMenuPage : MonoBehaviour
 {
     public string BattleSceneName;
+    public string NewBattleSceneName;
     public Button PlayButton;
+    public Button NewPlayButton;
     public LoadingLogicPage LoadingLogic;
 
     private void Awake()
     {
         PlayButton.onClick.AddListener(PlayGame);
+        NewPlayButton.onClick.AddListener(PlayNewGame);
     }
 
     private void PlayGame()
     {
         LoadingLogic.LoadScene(BattleSceneName);
     }
+
+    private void PlayNewGame()
+    {
+        LoadingLogic.LoadScene(NewBattleSceneName);
+    }
+
 }
